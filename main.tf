255a3f1f37e96da3407cf75a2b9e092a35fb316a
VICENTE0777 <vic.alarcona@duocuc.cl>
2026-04-17 02:06:35 -0400

    fix: format terraform

--- main.tf
+++ main.tf
@@ -58,8 +58,8 @@ resource "aws_iam_role" "vpc_flow_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
       Principal = {
         Service = "vpc-flow-logs.amazonaws.com"
       }
@@ -146,8 +146,8 @@ resource "aws_iam_role" "ec2_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
       Principal = {
         Service = "ec2.amazonaws.com"
       }
